ef11e22c50109647f5327db27f73b9707783f179
Yoshifumi Hosoya <yoshifumi.hosoya@gmail.com>
2013-03-10 22:29:18 +0900

    modify:setup.sh added .orig

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ DOT_FILES=( .zshrc .emacs.d .gitconfig .gitignore .vimrc .tmux.conf .bashrc .pro
 
 for file in ${DOT_FILES[@]}
 do
+      mv $HOME/$file $HOME/$file.orig
       ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
