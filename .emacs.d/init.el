b0dc6c7da0df09c15e03025d6b3ba13804055c80
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2013-03-13 01:43:12 +0900

    emacs font monaco

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,7 +121,7 @@
 
 ;; フォント Linux
 ;(set-frame-font "Ricty-11")
-;(set-frame-font "Monaco-10")
+(set-frame-font "Monaco-10")
 
 ;;;; カラーテーマ
 ;(require 'color-theme)
@@ -232,3 +232,16 @@
 ;;;多重起動の防止
 (server-start)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (deeper-blue))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
