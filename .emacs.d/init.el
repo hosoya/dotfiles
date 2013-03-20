ae3410aed1aa26a8d9bde5002fe896ba74dfe385
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2013-03-20 11:02:58 +0900

    emacs juge os/system

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,14 +3,6 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
 (add-to-list 'load-path "~/.emacs.d/auto-install/")
 (add-to-list 'load-path "~/.emacs.d/elpa")
-(add-hook 'c-mode-hook
-	  '(lambda()
-	     (c-set-style "bsd")
-	     (setq c-basic-offset 8)
-	     (setq tab-width c-basic-offset)
-	     (setq indent-tabs-mode t)))
-
-(setq ruby-deep-indent-paren-style nil)
 
 ;================================================================
 ;; キーバインド
@@ -42,16 +34,6 @@
 (auto-install-update-emacswiki-package-name t)
 (auto-install-compatibility-setup)
 
-
-;================================================================
-;; mozc 日本語入力
-;================================================================
-(require 'mozc)
-(set-language-environment "Japanese")
-(setq default-input-method "japanese-mozc")
-(global-set-key (kbd "M-`") 'toggle-input-method)
-
-
 ;================================================================
 ;; 言語/文字コード
 ;================================================================
@@ -63,6 +45,21 @@
 (set-buffer-file-coding-system 'utf-8-unix)
 
 ;================================================================
+;; indent
+;================================================================
+(add-hook 'c-mode-hook
+	  '(lambda()
+	     (c-set-style "bsd")
+	     (setq c-basic-offset 8)
+	     (setq tab-width c-basic-offset)
+	     (setq indent-tabs-mode t)))
+
+(setq ruby-deep-indent-paren-style nil)
+
+;;タブ幅を 8 に設定
+(setq-default tab-width 8)
+
+;================================================================
 ;; 表示
 ;================================================================
 (display-time)
@@ -72,39 +69,16 @@
 ;;; 行番号を左に表示
 (require 'linum)
 (global-linum-mode t)      ; デフォルトで linum-mode を有効にする
-;;タブ幅を 8 に設定
-(setq-default tab-width 8)
-
-;;; メニューバー、スクロールバーを消す
-(tool-bar-mode -1)
-(toggle-scroll-bar nil)
 
 ;;; 対応する括弧を表示させる
 (show-paren-mode 1)
 
-;; 画像ファイルを表示
-(auto-image-file-mode t)
-
 ;; カーソルの点滅を止める
 (blink-cursor-mode 0)
-
-;; 選択領域の色
-(set-face-background 'region "#842")
 
 ;; 行末の空白を強調表示
 (setq-default show-trailing-whitespace t)
 (set-face-background 'trailing-whitespace "#b14770")
-
-;; フォント Linux
-;(set-frame-font "Ricty-11")
-(set-frame-font "Monaco-10")
-
-;;;; カラーテーマ
-(require 'color-theme)
-(color-theme-dark-laptop)
-
-;; フレームの透明度
-(set-frame-parameter (selected-frame) 'alpha '(0.90))
 
 ;; 現在の関数名を表示
 (which-function-mode 1)
@@ -133,10 +107,6 @@
 ;================================================================
 ;; misc
 ;================================================================
-;; cua-mode 矩形選択 C-RETで起動 M-x cua-modeでenabledにする
-(cua-mode t)
-(setq cua-enable-cua-keys nil) ;; 変なキーバインド禁止
-
 ;; タイトルバーにファイルのフルパス表示
 (setq frame-title-format
       (format "%%f - Emacs@%s" (system-name)))
@@ -147,9 +117,8 @@
 ;フレーム版
 (require 'sr-speedbar)
 
-;;; *.~ とかのバックアップファイルを作らない
+;;; バックアップファイルを作らない
 (setq make-backup-files nil)
-;;; .#* とかのバックアップファイルを作らない
 (setq auto-save-default nil)
 
 ;; Ctl + Mouse左ボタン  buffer menu ポップアップを無効
@@ -168,19 +137,48 @@
 
 ;; 履歴を次回Emacs起動時にも保存する
 (savehist-mode 1)
+
 ;;; ファイル内のカーソル位置を記憶する
 (setq-default save-place t)
 (require 'saveplace)
-
-; クリップボードとキルリングを同期させる
-(cond (window-system
-       (setq x-select-enable-clipboard t)
-       ))
 
 ;; diredから"r"でファイル名をインライン編集する
 (require 'dired-x)
 (require 'wdired)
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 
-;;;多重起動の防止
-(server-start)
+;================================================================
+;; Emacs の種類バージョンを判別するための変数を定義
+;; (defun x->bool (elt) (not (not elt)))
+;; (defvar emacs22-p (equal emacs-major-version 22))
+;; (defvar emacs23-p (equal emacs-major-version 23))
+;; (defvar emacs24-p (equal emacs-major-version 24))
+;; (defvar darwin-p (eq system-type 'darwin))
+;; (defvar ns-p (featurep 'ns))
+;; (defvar mac-p (and (eq window-system 'mac) (or emacs23-p emacs24-p)))
+;; (defvar linux-p (eq system-type 'gnu/linux))
+;; (defvar nt-p (eq system-type 'windows-nt))
+
+;; OS,システム毎の設定
+(if (eq system-type 'gnu/linux)
+    (require 'mozc)
+  (set-language-environment "Japanese")
+  (setq default-input-method "japanese-mozc")
+  (global-set-key (kbd "M-`") 'toggle-input-method)
+  ;; フォント linux ;(set-frame-font "Ricty-11")
+  (set-frame-font "Monaco-10")
+  )
+
+(if window-system
+    (progn
+      (auto-image-file-mode t) ;; 画像ファイルを表示
+      (cua-mode t) ;; cua-mode 矩形選択 C-RETで起動 M-x cua-modeでenabledにする
+      (setq cua-enable-cua-keys nil) ;; 変なキーバインド禁止
+      (setq x-select-enable-clipboard t); クリップボードとキルリングを同期させる
+      (require 'color-theme)
+      (color-theme-dark-laptop)
+      (set-frame-parameter (selected-frame) 'alpha '(0.90)) ;; フレームの透明度
+      (tool-bar-mode -1) ;;; メニューバー、スクロールバーを消す
+      (toggle-scroll-bar nil)
+      (server-start) ;;;多重起動の防止
+      ))
