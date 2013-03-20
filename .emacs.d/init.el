9b28c6ac05bc5518c0668c6abb9a72fe56edd2cf
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2013-03-20 09:30:27 +0900

    temp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,80 +1,51 @@
 ;;; パス
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/lisp")
-;(add-to-list 'load-path "~/.emacs.d/auto-install")
 (add-to-list 'load-path "~/.emacs.d/elpa")
 (add-to-list 'load-path "~/.emacs.d/helm")
 
-;; (add-hook 'c-mode-hook
-;;         '(lambda()
-;;                 (c-set-style "bsd")
-;;                 (setq c-basic-offset 8)
-;;                 (setq tab-width c-basic-offset)
-;;                 (setq indent-tabs-mode t)))
+(add-hook 'c-mode-hook
+	  '(lambda()
+	     (c-set-style "bsd")
+	     (setq c-basic-offset 8)
+	     (setq tab-width c-basic-offset)
+	     (setq indent-tabs-mode t)))
 
-;; (setq ruby-deep-indent-paren-style nil)
+(setq ruby-deep-indent-paren-style nil)
 
 
-;===============================================================================
+;=============================================================
+;; 24
+;=============================================================
+(global-set-key (kbd “C-x C-b”) ‘ibuffer)
+(autoload ‘ibuffer “ibuffer” “List buffers.” t)
+
+;=============================================================
 ;; キーバインド
-;===============================================================================
+;=============================================================
 (define-key global-map "\C-h" 'delete-backward-char) ; 削除
-(define-key global-map "\M-?" 'help-for-help)        ; ヘルプ
-(define-key global-map "\C-z" 'undo)                 ; undo
-
-
-;(require 'package)
-;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-; (package-initialize)
-
-;===============================================================================
-;; anytihng
-;===============================================================================
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-;; (when
-;; 	(load2
-;; 	 (expand-file-name "~/.emacs.d/elpa/package.el"))
-;;   (package-initialize))
-
-;;; moccur
-;(require 'moccur-edit)
-;(setq moccur-split-word t)
+(define-key global-map "\M-?" 'help-for-help)	     ; ヘルプ
+(define-key global-map "\C-z" 'undo)		     ; undo
 
 ;;; anytihng-el
 ;(require 'anything-startup)
 (require 'helm-config)
+(require 'helm-command)
+;(require 'helm-descbinds)
 (helm-mode 1)
 
-; auto-async-byte-compile.el
-;(require 'auto-async-byte-compile)
-;(setq auto-async-byte-compile-exclude-files-regexp "~/.emacs.d/junk/")
-;(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
-
-; auto-install
-;(require 'auto-install)
-;(add-to-list 'load-path "~/.emacs.d/auto-install")
-;(setq auto-install-directory "~/.emacs.d/auto-install/")
-;(auto-install-update-emacswiki-package-name t)
-;(auto-install-compatibility-setup)
-
-
-;===============================================================================
+;=============================================================
 ;; mozc 日本語入力
-;===============================================================================
+;=============================================================
 ;(require 'mozc)
 ;(set-language-environment "Japanese")
 ;(setq default-input-method "japanese-mozc")
 ;(global-set-key (kbd "M-`") 'toggle-input-method)
 
 
-;===============================================================================
+;=============================================================
 ;; 言語/文字コード
-;===============================================================================
+;=============================================================
 (prefer-coding-system 'utf-8-unix)
 ;(set-language-environment "Japanese")
 (set-locale-environment "en_US.UTF-8") ; "ja_JP.UTF-8"
@@ -82,20 +53,16 @@
 (set-selection-coding-system 'utf-8-unix)
 (set-buffer-file-coding-system 'utf-8-unix)
 
-;===============================================================================
+;=============================================================
 ;; 表示
-;===============================================================================
+;=============================================================
 (display-time)
 ;;; 行番号・桁番号を表示させる
 (line-number-mode 1)
 (column-number-mode 1)
 ;;; 行番号を左に表示
 (require 'linum)
-(global-linum-mode t)      ; デフォルトで linum-mode を有効にする
-;;; リージョンに色をつける
-;(transient-mark-mode 1)
-;;; おりかえし禁止
-;(setq truncate-partial-width-windows t)
+(global-linum-mode t)	   ; デフォルトで linum-mode を有効にす
 ;;タブ幅を 8 に設定
 (setq-default tab-width 8)
 
@@ -106,9 +73,6 @@
 
 ;;; 対応する括弧を表示させる
 (show-paren-mode 1)
-
-; current highlight line
-;(global-hl-line-mode)
 
 ;; 画像ファイルを表示
 (auto-image-file-mode t)
@@ -121,9 +85,6 @@
 ;; diffのオプション
 ;(setq diff-switches '("-u" "-p" "-N"))
 
-;; 選択領域の色
-;(set-face-background 'region "#842")
-
 ;; 行末の空白を強調表示
 (setq-default show-trailing-whitespace t)
 (set-face-background 'trailing-whitespace "#b14770")
@@ -132,12 +93,8 @@
 ;(set-frame-font "Ricty-11")
 ;(set-frame-font "Monaco-10", nil t)
 
-;;;; カラーテーマ
-;(require 'color-theme)
-;(color-theme-dark-laptop)
-
 ;; フレームの透明度
-(set-frame-parameter (selected-frame) 'alpha '(0.85))
+(set-frame-parameter (selected-frame) 'alpha '(0.90))
 
 ;; 現在の関数名を表示
 (which-function-mode 1)
@@ -145,49 +102,27 @@
 ;; スタートアップ非表示
 (setq inhibit-startup-screen t)
 
-;; diffの表示方法を変更
-;===================================================
-;; (defun diff-mode-setup-faces ()
-;;   ;; 追加された行は緑で表示
-;;   (set-face-attribute 'diff-added nil
-;;                       :foreground "white" :background "light green")
-;;   ;; 削除された行は赤で表示
-
-;;   (set-face-attribute 'diff-removed nil
-;;                       :foreground "white" :background "light red")
-;;   ;; 文字単位での変更箇所は色を反転して強調
-;;   (set-face-attribute 'diff-refine-change nil
-;;                       :foreground nil :background nil
-;;                       :weight 'bold :inverse-video t))
-;; (add-hook 'diff-mode-hook 'diff-mode-setup-faces)
-
-;; ;; diffを表示したらすぐに文字単位での強調表示も行う
-;; (defun diff-mode-refine-automatically ()
-;;   (diff-auto-refine-mode t))
-;; (add-hook 'diff-mode-hook 'diff-mode-refine-automatically)
-
 ;; setting for diff-mode
 (require 'diff-mode)
 
 (set-face-attribute 'diff-added-face nil
-                    :background nil :foreground "green"
-                    :weight 'normal)
+		    :background nil :foreground "green"
+		    :weight 'normal)
 (set-face-attribute 'diff-removed-face nil
-                    :background nil :foreground "firebrick1"
-                    :weight 'normal)
+		    :background nil :foreground "firebrick1"
+		    :weight 'normal)
 
 (set-face-attribute 'diff-file-header-face nil
-                    :background nil :weight 'extra-bold)
+		    :background nil :weight 'extra-bold)
 
 (set-face-attribute 'diff-hunk-header-face nil
-                    :foreground "chocolate4"
-                    :background "white" :weight 'extra-bold
-                    :underline t :inherit nil)
+		    :foreground "chocolate4"
+		    :background "white" :weight 'extra-bold
+		    :underline t :inherit nil)
 
-
-;===============================================================================
+;============================================================
 ;; misc
-;===============================================================================
+;============================================================
 ;; cua-mode 矩形選択 C-RETで起動 M-x cua-modeでenabledにする
 ;; http://e-arrows.sakura.ne.jp/2010/02/vim-to-emacs.html
 (cua-mode t)
@@ -240,8 +175,6 @@
 
 ;;;多重起動の防止
 (server-start)
-
-(autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
