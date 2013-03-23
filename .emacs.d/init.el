043768f866069743bef3ec15b8eb94280695e3dc
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2013-03-23 20:33:48 +0900

    emacs update yasnippet

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,7 +52,28 @@
 ;================================================================
 ;; yasnippet
 ;================================================================
+;; yasnippetを置いているフォルダにパスを通す
+;(add-to-list 'load-path
+;             (expand-file-name "~/.emacs.d/elisp/yasnippet"))
 (require 'yasnippet)
+;; ~/.emacs.d/にsnippetsというフォルダを作っておきましょう
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets" ;; 作成するスニペットはここに入る
+        "~/.emacs.d/elpa/yasnippet-20130218.2229/snippets" ;; 最初から入っていたスニペット(省略可能)
+        ))
+(yas-global-mode 1)
+
+;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
+;; (setqだとtermなどで干渉問題ありでした)
+;; もちろんTAB以外でもOK 例えば "C-;"とか
+(custom-set-variables '(yas-trigger-key "TAB"))
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
 ;================================================================
 ;; 言語/文字コード
