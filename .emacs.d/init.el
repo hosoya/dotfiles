388279867aa305f9ad523cd7f6ac9564bca9392f
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2013-04-04 02:06:42 +0900

    emacs el-get add

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -50,6 +50,20 @@
 (require 'melpa)
 
 ;================================================================
+;; el-get
+;================================================================
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync)
+
+;================================================================
 ;; yasnippet
 ;================================================================
 ;; yasnippetを置いているフォルダにパスを通す
