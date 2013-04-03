50d83130bf42cbc4745643acc7b6254547496c8f
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2013-04-04 00:45:06 +0900

    emacs diff color

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -142,23 +142,40 @@
 (setq default-frame-alist initial-frame-alist)
 
 ;; diffの表示方法を変更
-(defun diff-mode-setup-faces ()
-  ;; 追加された行は緑で表示
-  (set-face-attribute 'diff-added nil
-                      :foreground "white" :background "dark green")
-  ;; 削除された行は赤で表示
-  (set-face-attribute 'diff-removed nil
-                      :foreground "white" :background "dark red")
-  ;; 文字単位での変更箇所は色を反転して強調
-  (set-face-attribute 'diff-refine-change nil
-                      :foreground nil :background nil
-                      :weight 'bold :inverse-video t))
-(add-hook 'diff-mode-hook 'diff-mode-setup-faces)
+;; (defun diff-mode-setup-faces ()
+;;   ;; 追加された行は緑で表示
+;;   (set-face-attribute 'diff-added nil
+;;                       :foreground "white" :background "dark olive green")
+;;   ;; 削除された行は赤で表示
+;;   (set-face-attribute 'diff-removed nil
+;;                       :foreground "white" :background "PaleVioletRed4")
+;;   ;; 文字単位での変更箇所は色を反転して強調
+;;   (set-face-attribute 'diff-refine-change nil
+;;                       :foreground nil :background nil
+;;                       :weight 'bold :inverse-video t))
+;; (add-hook 'diff-mode-hook 'diff-mode-setup-faces)
 
-;; diffを表示したらすぐに文字単位での強調表示も行う
-(defun diff-mode-refine-automatically ()
-  (diff-auto-refine-mode t))
-(add-hook 'diff-mode-hook 'diff-mode-refine-automatically)
+;; ;; diffを表示したらすぐに文字単位での強調表示も行う
+;; (defun diff-mode-refine-automatically ()
+;;   (diff-auto-refine-mode t))
+;; (add-hook 'diff-mode-hook 'diff-mode-refine-automatically)
+
+;; diffの表示方法を変更 その２
+(require 'diff-mode)
+(set-face-attribute 'diff-added-face nil
+		    :background nil :foreground "green"
+		    :weight 'normal)
+(set-face-attribute 'diff-removed-face nil
+		    :background nil :foreground "red"
+		    :weight 'normal)
+
+(set-face-attribute 'diff-file-header-face nil
+		    :background nil :weight 'extra-bold)
+
+(set-face-attribute 'diff-hunk-header-face nil
+		    :foreground "chocolate4"
+		    :background "white" :weight 'extra-bold
+		    :underline t :inherit nil)
 
 ;================================================================
 ;; misc
