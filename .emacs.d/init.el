3ebafc76d35f8b734a51ffb2341bd3a64438b680
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2013-04-04 00:24:07 +0900

    emacs color theme, diff color changed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -132,24 +132,6 @@
 ;; 折り返さない
 (setq-default truncate-lines t)
 
-;; diffの表示方法を変更
-;===================================================
-(require 'diff-mode)
-(set-face-attribute 'diff-added-face nil
-                    :background nil :foreground "green"
-                    :weight 'normal)
-(set-face-attribute 'diff-removed-face nil
-                    :background nil :foreground "firebrick1"
-                    :weight 'normal)
-
-(set-face-attribute 'diff-file-header-face nil
-                    :background nil :weight 'extra-bold)
-
-(set-face-attribute 'diff-hunk-header-face nil
-                    :foreground "chocolate4"
-                    :background "white" :weight 'extra-bold
-                    :underline t :inherit nil)
-
 ;;起動時のフレームサイズを設定する
 (setq initial-frame-alist
       (append (list
@@ -158,6 +140,25 @@
 	       )
 	      initial-frame-alist))
 (setq default-frame-alist initial-frame-alist)
+
+;; diffの表示方法を変更
+(defun diff-mode-setup-faces ()
+  ;; 追加された行は緑で表示
+  (set-face-attribute 'diff-added nil
+                      :foreground "white" :background "dark green")
+  ;; 削除された行は赤で表示
+  (set-face-attribute 'diff-removed nil
+                      :foreground "white" :background "dark red")
+  ;; 文字単位での変更箇所は色を反転して強調
+  (set-face-attribute 'diff-refine-change nil
+                      :foreground nil :background nil
+                      :weight 'bold :inverse-video t))
+(add-hook 'diff-mode-hook 'diff-mode-setup-faces)
+
+;; diffを表示したらすぐに文字単位での強調表示も行う
+(defun diff-mode-refine-automatically ()
+  (diff-auto-refine-mode t))
+(add-hook 'diff-mode-hook 'diff-mode-refine-automatically)
 
 ;================================================================
 ;; misc
@@ -253,10 +254,11 @@
       (toggle-scroll-bar nil)
       (require 'color-theme)
       (color-theme-initialize)
-      (require 'color-theme)
-      (require 'color-theme-tangotango)
-      (require 'color-theme-wombat)
-      (require 'color-theme-sanityinc-tomorrow)))
+      (color-theme-dark-laptop)
+      ;; (require 'color-theme-tangotango)
+      ;; (require 'color-theme-wombat)
+      ;; (require 'color-theme-sanityinc-tomorrow)
+      ))
 
 ;; 多重起動の防止
 (require 'server)
