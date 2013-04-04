4675ec2e6aa52f1ee348cc2f628e2ae85f09e101
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2013-04-04 22:45:39 +0900

    emacs el-get removed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -50,32 +50,6 @@
 (require 'melpa)
 
 ;================================================================
-;; el-get
-;================================================================
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(el-get 'sync)
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
-
-(el-get 'sync)
-
-;================================================================
 ;; yasnippet
 ;================================================================
 ;; yasnippetを置いているフォルダにパスを通す
